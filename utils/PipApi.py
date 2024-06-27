5cba965af0e204a4abbaa245ad264bfb37da7a74
MaxCosmos <renyh2001@outlook.com>
2024-06-27 21:41:52 +0800

    重构代码

--- utils/PipApi.py
+++ utils/PipApi.py
@@ -1,28 +1,37 @@
 import subprocess
 
 
-def get_pip_list():
+class PipApi:
     """
-    获取pip列表
-    :return: 列表数据对象
+    所有获取Pip数据的接口
     """
-    result = subprocess.run(['pip', 'list'], capture_output=True, text=True)
 
-    lines = result.stdout.strip().split('\n')
-    package_lines = lines[2:]
-    packages = []
+    def __init__(self):
+        self.pip = 'pip'
 
-    for line in package_lines:
-        parts = line.split()
-        name = parts[0]
-        version = parts[1]
-        packages.append({
-            'name': name,
-            'version': version
-        })
+    def get_pip_list(self):
+        """
+        获取pip列表
+        :return: 列表数据对象
+        """
+        result = subprocess.run([self.pip, 'list'], capture_output=True, text=True)
 
-    return packages
+        lines = result.stdout.strip().split('\n')
+        package_lines = lines[2:]
+        packages = []
+
+        for line in package_lines:
+            parts = line.split()
+            name = parts[0]
+            version = parts[1]
+            packages.append({
+                'name': name,
+                'version': version
+            })
+
+        return packages
 
 
 if __name__ == "__main__":
-    print(get_pip_list())
+    pipApi = PipApi()
+    print(pipApi.get_pip_list())
