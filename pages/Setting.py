dfa8f71793d892f12abb84b2df8a4c1e083f9c9b
Fang <zhengjun.fang0428@gmail.com>
2024-07-04 00:48:23 +1000

    完善镜像源检查功能

--- pages/Setting.py
+++ pages/Setting.py
@@ -12,30 +12,31 @@ class Setting:
         self.entry_var = tk.StringVar()
         self.frame = frame
         self.file_choose_button = ttk.Button(self.frame, text="选择文件", command=self.file_path)
-        self.file_choose_button.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-        self.entry = ttk.Entry(self.frame, width=30, style=PRIMARY, state="readonly", textvariable=self.entry_var)
-        self.entry.grid(row=0, column=1, padx=10, pady=10, sticky="w")
-        self.entry = ttk.Entry(self.frame, width=30, style=PRIMARY)
-        self.entry.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+        self.entry_file = ttk.Entry(self.frame, width=30, style=PRIMARY, state="readonly", textvariable=self.entry_var)
+        self.entry_url = ttk.Entry(self.frame, width=30, style=PRIMARY)
         self.pip_mirror_label = ttk.Label(self.frame, text="镜像源地址")
-        self.pip_mirror_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
         self.ping_result_label = ttk.Label(self.frame, text="", foreground="red")
-        info = self.process_url()
-        self.ping_result_label.config(text=info)
-
-        self.ping_result_label.grid(row=1, column=3, padx=10, pady=10, sticky="w")
-
+        self.ping_button = ttk.Button(self.frame, text="ping", command=self.process_url)
         self.theme_choose_list = ttk.Combobox(self.frame,
                                               values=["cosmo", "cyborg", "darkly", "flatly", "journal", "litera",
                                                       "lumen", "lux", "materia", "minty", "pulse", "sandstone",
                                                       "simplex", "sketchy", "slate", "solar", "spacelab", "superhero",
                                                       "united", "yeti"])
-        self.theme_choose_list.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+
         self.theme_choose_label = ttk.Label(self.frame, text="主题")
-        self.theme_choose_label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
         self.save_button = ttk.Button(self.frame, text="保存")
-        self.save_button.grid(row=3, column=0, padx=10, pady=10, sticky="w")
         self.cancel_button = ttk.Button(self.frame, text="取消")
+
+        #     布局
+        self.file_choose_button.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+        self.entry_file.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+        self.entry_url.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+        self.pip_mirror_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        self.ping_button.grid(row=1, column=2, padx=10, pady=10, sticky="w")
+        self.ping_result_label.grid(row=1, column=3, padx=10, pady=10, sticky="w")
+        self.theme_choose_list.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+        self.theme_choose_label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+        self.save_button.grid(row=3, column=0, padx=10, pady=10, sticky="w")
         self.cancel_button.grid(row=3, column=1, padx=10, pady=10, sticky="w")
 
     def file_path(self):
@@ -48,14 +49,14 @@ class Setting:
         return time
 
     def process_url(self):
-        url = self.entry_var.get()
+        url = self.entry_url.get()
         if url == "":
             self.ping_result_label.config(text="请输入地址")
         else:
-
             url_pattern = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+')
             if url_pattern.match(url):
+                self.ping_result_label.config(text="")
                 result = self.ping(url)
-                return result
+                self.ping_result_label.config(text=f"{result}s")
             else:
                 self.ping_result_label.config(text="请输入正确的地址")
